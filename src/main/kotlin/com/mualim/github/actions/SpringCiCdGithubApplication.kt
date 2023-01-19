bf7434461af760a31c3bf3aff0f188f71355d93b
Mu'alim Syahrido <IDS.MUALIMS@xl.co.id>
2023-01-19 21:32:31 +0700

    Initial commit

---
+++ src/main/kotlin/com/mualim/github/actions/SpringCiCdGithubApplication.kt
@@ -0,0 +1,19 @@
+package com.mualim.github.actions
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api/v1")
+class SpringCiCdGithubApplication {
+    @GetMapping("/welcome")
+    fun welcome(): String = "Welcome to the Spring"
+}
+
+fun main(args: Array<String>) {
+    runApplication<SpringCiCdGithubApplication>(*args)
+}
---
+++ src/test/kotlin/com/mualim/github/actions/SpringCiCdGithubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mualim.github.actions
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCiCdGithubApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
